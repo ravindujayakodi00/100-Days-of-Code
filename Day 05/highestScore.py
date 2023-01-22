20da79f98bcd47ec57db77b7114465d2d21292fb
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-01-22 23:00:37 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Day 05/highestScore.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+index = 0
+highestScore = 0
+for x in student_scores:
+    if x > student_scores[index] and x > highestScore:
+        highestScore = x
+print(f"The highest score in the class is: {highestScore}")
+
+
+
+
+
+
+
+
