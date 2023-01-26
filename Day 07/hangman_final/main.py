bbf125a774342a24c47c42a87b21ef20b01dba5f
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-01-26 23:24:01 +0530

    Merge remote-tracking branch 'origin/main'

--- Day 07/hangman_final/main.py
+++ Day 07/hangman_final/main.py
@@ -4,6 +4,7 @@ import random
 import hangman_words
 import hangman_art
 
+
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
