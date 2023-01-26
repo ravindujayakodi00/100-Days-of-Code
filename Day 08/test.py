6fba948b7445af21fa56a934c97dd1ad2436196d
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-01-27 00:07:53 +0530

    Merge remote-tracking branch 'origin/main'

--- Day 08/test.py
+++ Day 08/test.py
@@ -1,4 +1,11 @@
 def greet(name):
     print(f"Hello {name}")
 
-greet("Ravindu")
+# greet("Ravindu")
+
+# Key word arguments
+
+def greetWith(location, name):
+    print(f"Hello {name} How is it going in {location}")
+
+greetWith(location="Kandy" , name="Ravindu")
