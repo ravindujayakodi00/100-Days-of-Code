24e5a06e070791d7136d38d631455292235bb8e2
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-01-27 17:25:45 +0530

    Merge remote-tracking branch 'origin/main'

--- Day 08/test.py
+++ Day 08/test.py
@@ -8,4 +8,18 @@ def greet(name):
 def greetWith(location, name):
     print(f"Hello {name} How is it going in {location}")
 
-greetWith(location="Kandy" , name="Ravindu")
+# greetWith(location="Kandy" , name="Ravindu")
+
+# prime number checker
+def prime_checker(number):
+    is_prime = True
+    for x in range (2,number):
+        if number % x == 0:
+            is_prime = False
+    if is_prime == True:
+        print("This is a Prime Number")
+    else:
+        print("This is not a prime number")
+
+number = int(input("Enter a Number: "))
+prime_checker(number)
