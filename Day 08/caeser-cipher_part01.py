dfdfdb7bee7ef59cf862240011c5d6d1ad0cbf4d
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-01-29 23:11:49 +0530

    Merge remote-tracking branch 'origin/main'

--- Day 08/caeser-cipher_part01.py
+++ Day 08/caeser-cipher_part01.py
@@ -1,34 +1,46 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
     #e.g.
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
-    letters = []
-    for x in text:
-        letters.append(x)
-    print(letters)
-    letter = 0
-    for x in letters:
-        if x != ' ':
-            position = alphabet.index(x)
-            new_position = position + shift
-            letters[letter] = alphabet[new_position]
-        letter += 1
 
-    print(letters)
-    encoded_text = ""
-    for x in letters:
-        encoded_text = encoded_text + x
-    print(encoded_text)
+# def encrypt(plain_text, shift_amount):
+#     letters = []
+#     for x in text:
+#         letters.append(x)
+#     letter = 0
+#     for x in letters:
+#         if x != ' ':
+#             position = alphabet.index(x)
+#             new_position = position + shift
+#             letters[letter] = alphabet[new_position]
+#         letter += 1
+#
+#     encoded_text = ""
+#     for x in letters:
+#         encoded_text = encoded_text + x
+#     print(encoded_text)
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        if letter != ' ':
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            cipher_text += new_letter
+        else:
+            cipher_text += ' '
+    print(cipher_text)
 
     ##HINT: How do you get the index of an item in a list:
     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
