80b92c357d3a8d713658066f7d3be2b8006dbf24
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-02-03 23:02:43 +0530

    Days in month

---
+++ Day 10/test.py
@@ -0,0 +1,16 @@
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+#
+# def my_function(num1, num2 ):
+#     return num1 * num2
+#
+# output = my_function(num1, num2)
+#
+# print(output)
+
+# test 1
+
+def format_name(fname, lname):
+    return fname.title() + " " + lname.title()
+
+print(format_name("ravindu", "jaYakodi"))
