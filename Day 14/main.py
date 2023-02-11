7803444a30ce325807683f6a5dfcdf694ee66f5a
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-02-11 23:22:24 +0530

    Higher - Lower game

--- Day 14/main.py
+++ Day 14/main.py
@@ -5,6 +5,12 @@ import random
 def generate_random():
     return random.choice(data)
 
+def format(account):
+    name = account["name"]
 
+
+def game():
+    print(logo)
+    generate_random()
 
 
