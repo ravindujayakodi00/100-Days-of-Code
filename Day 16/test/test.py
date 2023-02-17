fa37d80da53d13dbc75e36ce92ed2026ac171d6d
ravindujayakodi00 <ravindujayakodi00@gmail.com>
2023-02-17 23:00:37 +0530

    OOP

---
+++ Day 16/test/test.py
@@ -0,0 +1,9 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squitle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "r"
+print(table)
